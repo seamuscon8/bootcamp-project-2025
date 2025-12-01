cf4c3bc1bf22f87b8e6ba8148f946b566a7c9c1b
Seamus <seamusrconnolly@gmail.com>
2025-12-01 12:11:24 -0800

    Set up milestone-4

---
+++ milestone-4/.next/dev/static/chunks/0a04a_bootcamp-project-2025_milestone-4_src_app_blogs_[slug]_page_tsx_e696756d._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/3a166_next_dist_f458b745._.js",
+  "static/chunks/BootCamp_bootcamp-project-2025_milestone-4_src_components_2788becb._.css"
+],
+    source: "dynamic"
+});
